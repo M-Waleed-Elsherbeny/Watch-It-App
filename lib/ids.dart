637d8bbea2bfe30155cdedee082d712b5549a2d7
Mphammed Waleed <m.elsherbeny100@gmail.com>
2026-02-21 11:21:17 +0200

    finish app

---
+++ lib/ids.dart
@@ -0,0 +1,14 @@
+List<Map<String, dynamic>> ids = [
+  {"title": "ليه تتعلم بايثون في 2026", "id": "PBo_7cvzFWA"},
+  {"title": "أطلقت تطبيق إسلامي متكامل ومجاني… شاركه في الخير الآن", "id": "f_eVZlv-IWQ"},
+  {
+    "title": "لا تتعلم برمجة في 2026 قبل ما تشوف الفيديو ده",
+    "id": "5GNnnax4mmk",
+  },
+  {
+    "title": "مرتبات المبرمجين في مصر 2026 | الواقع من قلب السوق",
+    "id": "fO4Ha_aMmDs",
+  },
+  {"title": "Flutter في خمس دقايق", "id": "7UAObj6ta1k"},
+  {"title": "What is State in Bloc & Cubit", "id": "722IPTE_qAo"},
+];
---
+++ lib/watch_it_view.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:watch_it/ids.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class WatchItView extends StatelessWidget {
+  const WatchItView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Watch It',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () {
+              // Handle search action
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.notifications, color: Colors.white),
+            onPressed: () {
+              // Handle search action
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: ids.length,
+        itemBuilder: (context, index) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              YoutubePlayer(
+                controller: YoutubePlayerController(
+                  initialVideoId: ids[index]['id'],
+                  flags: const YoutubePlayerFlags(autoPlay: false, mute: false),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                ids[index]['title'],
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
